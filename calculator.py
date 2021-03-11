f886eda5c02e7af903d0e490ea53e80ae2faa7d7
Alex Young <younga6@oregonstate.edu>
2021-03-11 08:49:50 -0800

    fixed linter

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
+# CS362 In-Class Activity calculator ci
+# Alex Young
+# 3/11/2021
+# Run this file using python3 calculator.py
+# This program holds calculator funtions
 
 
-.def add(a, b):
+def add(a, b):
     return a + b
 
 
 def subtract(a, b):
     return a - b
-
