39d38840bbfa351f057483e1933f6b41f1a4d105
Alex Young <younga6@oregonstate.edu>
2021-03-11 08:57:37 -0800

    added unit tests for add and subtract

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+# CS362 test_calculator
+# Alex Young
+# 3/11/2021
+# Run this file using python3 test_calculator.py
+# This program holds funtions to unit test for the functions in calculator
+
+import unittest
+import calculator
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
